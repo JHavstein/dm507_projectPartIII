10fe1aeb08d3df421bcac2f677453c5e6b157eb8
Jonas Alexander Havstein Eriksen <jahe@Jonass-MacBook-Air.local>
2018-04-25 11:02:12 +0200

    No major changes

---
+++ Huffmantree.java
@@ -0,0 +1,15 @@
+public class Huffmantree{
+	private Node root; 
+    private int elms;
+    private int i = 0;
+	
+	// Constructor method 
+	public Huffmantree(){
+        this.root = null;
+        this.elms = 0;
+	}
+	
+	public Huffmantree merge(Huffmantree a, Huffmantree b){
+		// TODO
+	}
+}
