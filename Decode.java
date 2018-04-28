d7236d78208f1039721dcf5716938b3613e3c12c
Jonas Alexander Havstein Eriksen <jahe@Jonass-MacBook-Air.local>
2018-04-28 23:05:18 +0200

    Decode.java added

---
+++ Decode.java
@@ -0,0 +1,13 @@
+import java.io.*;
+
+// Main class for the decoding  
+public class Decode{
+	public static void main(String[] args){
+		try{
+			
+		}
+		catch(IOException e){
+			e.printStackTrace(); 
+		}
+	}
+}
