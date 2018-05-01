71985ef41fbf5142024fecd66443f7c8ceaa21c5
Jonas Alexander Havstein Eriksen <jahe@Jonass-MacBook-Air.local>
2018-05-01 20:36:20 +0200

    Adding HashMap

--- Decode.java
+++ Decode.java
@@ -3,12 +3,13 @@ import java.io.FileOutputStream;
 import java.io.DataOutputStream; 
 import java.io.File;
 import java.io.IOException;
+import java.util.*;
 
 // Main class for the decoding  
 public class Decode{
 	public static void main(String[] args){
-		// Terminerer program, hvis fejlinput fra brugeren
 		if (args.length != 2){
+			// Terminerer program, hvis fejlinput fra brugeren
 			if(args.length == 1){
 				System.out.println("Programmet kræver præcis 2 argumenter. Du gav 1 argument. Terminerer program.");
 			}
@@ -20,11 +21,9 @@ public class Decode{
 		}
 		else{
 			try{
-				// Åbner inputstreams
+				// Åbner IOstreams
 				FileInputStream fileIn = new FileInputStream(args[0]);
             	BitInputStream bitIn = new BitInputStream(fileIn);
-				
-				// Åbner outputstreams
 				FileOutputStream fileOut = new FileOutputStream(args[1]); 
 				BitOutputStream bitOut = new BitOutputStream(fileOut); 
 					
@@ -46,20 +45,34 @@ public class Decode{
 				// Samme implementation af makeHuffmanTable() som i Encode.java
 				String[] table = makeHuffmanTable(n);
 				
-				// Dekoder tekst og skriver til output								
 				int nBit; 
 				String outByte; 
 				String temp = ""; 	
 				int charCounter = 0; // tæller hvor mange bytes, der er skrevet til output	
-						
+				
+				// Laver HashMap til at opbevare bitmønstre i
+				Map<Integer,String> byteMap = new HashMap<Integer,String>(256,100);
+				
+				// Læser Huffman-kodeord fra input og skriver til output	
+				// Første betingelse for while-løkken er, at der er mere at læse fra filen
+				// og at der der kun skrives det antal bytes til output som der var 
+				// i den originale fil, der blev komprimeret med Encode.java.	
 				while((nBit = bitIn.readBit()) != -1 & charCounter < totalChar){
 					// Læser input og konkatenerer i temp
 					temp = temp + String.valueOf(nBit); 
 					for(int i = 0; i < table.length; i++){
 						// Tjekker om temp passer med et af Huffman-kodeordene
 						if (table[i].equals(temp)){
-							// Laver int om til byte af fixed længde 8 
-							outByte = convertToBinary(i); 
+							// Tjekker om kodeorder er mødt før
+							if (byteMap.containsKey(i)){
+								// Gemmer bitmønster i byteMap 
+								outByte = byteMap.get(i); 
+							} 
+							else{
+								// Kodeordet er mødt før, så det slås op i byteMap
+								outByte = convertToBinary(i); 
+								byteMap.put(i, outByte); 
+							}
 							// Skriver bits fra byte bag Huffman kodeord til output
 							for (int r = 0; r < 8; r++){
 								int outputBit = Character.getNumericValue(outByte.charAt(r)); 
@@ -70,7 +83,8 @@ public class Decode{
 						}
 					}
 				}
-				// Lukker streams
+				
+				// Lukker IOstreams
 				bitIn.close();
 				bitOut.close(); 
 								
@@ -81,7 +95,7 @@ public class Decode{
 		}
 	}
 	
-	// Skal nok gøres non-static
+	// Samme implementation som i Encode.java
 	public static PQHeap makeHuffmanTree(int[] a){
 		PQHeap HuffmanTree = new PQHeap(a.length);
 		for (int i = 0; i < a.length; i++){
@@ -98,7 +112,8 @@ public class Decode{
 		}
 		return HuffmanTree; 
 	}
-
+	
+	// Samme implementation som i Encode.java
 	public static String[] makeHuffmanTable( PQHeap t ) {
 		String[] out = new String[256];
 		HuffmanTempTree huff = t.extractMin().data;
@@ -107,8 +122,7 @@ public class Decode{
 	
 	// Metode til at konvertere en int til 
 	// binær (d.v.s. 10-talssystem --> binært talsystem).
-	// i skal være i [0; 255].
-	// Output har fixed længde 8. 
+	// i skal være i [0; 255]. Output har fixed længde 8. 
 	public static String convertToBinary(int i){
 		int q; // kvotient 
 		int m; // rest 
