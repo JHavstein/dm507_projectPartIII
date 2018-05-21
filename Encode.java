7ef27cea4b373bb8ecb7558b30481d138ddea998
matblicher <33628301+matblicher@users.noreply.github.com>
2018-05-21 03:47:17 +0200

    Update Encode.java

--- Encode.java
+++ Encode.java
@@ -4,7 +4,10 @@ DM507, project part III
 	Jeanette Frieda Aviaya Sommer - jeaso17 - 08-05-1984
 	Jonas Alexander Havstein Eriksen - joeri15 - 16-02-1993
 */
-import java.io.*; 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 // Main class for the encoding part.
 public class Encode{
@@ -21,14 +24,20 @@ public class Encode{
 			System.exit(0);
 		}
 		else{
-			// Initializing Bytereader object which takes a File object as an argument
-			// for its constructor method.
-			ByteReader br = new ByteReader(new File(args[0]));
-			
+			//Building array containing the frequency of each byte in the file.
+			int[] freqTable = new int[256];
+                    	try(FileInputStream input = new FileInputStream(new File(args[0]))){
+            			int byteContent;
+            			while((byteContent = input.read()) != -1){
+                			freqTable[byteContent] = freqTable[byteContent] + 1; 
+            			}
+        		}
+        		catch (IOException e){
+            			e.printStackTrace();
+        		}
 			// Making Huffman tree and Huffman codes.
-			br.byteReader();
-			PQHeap n = makeHuffmanTree(br.getFrequencies());
-			String[] table = makeHuffmanTable(n);
+			String[] table = new Huffman(freqTable).makeCodes();
+			
 			FileInputStream input = null;
             		BitOutputStream outputFreq = null;
             		try{
@@ -40,8 +49,7 @@ public class Encode{
 				// Format: The frequencies are written as consecutive non-separated integers
 				// in range [0;255]. 
 				for (int i = 0; i <= 255; i++){
-					int out = br.freqAtIndex(i);
-					outputFreq.writeInt(out);
+					outputFreq.writeInt(freqTable[i]);
 				} 
 												
 				// Writing Huffman codes to the output file.
@@ -49,8 +57,7 @@ public class Encode{
                 		while((x = input.read())!= -1){
                     			String in = table[x];
                     			for (int i=0; i < in.length(); i++){
-                        		int out = Character.getNumericValue(in.charAt(i));
-                        		outputFreq.writeBit(out);
+                        			outputFreq.writeBit(Character.getNumericValue(in.charAt(i)));
                     			}
                 		}
             		}
@@ -77,44 +84,5 @@ public class Encode{
                 		} 
             		}
 		}
-	}
-	
-	// Method for making the a Huffman Tree from a PQHeap.
-	// The method takes an int[] as a parameter, which should
-	// be the frequency table created by the Bytereader object.
-	public static PQHeap makeHuffmanTree(int[] a) {
-		PQHeap HuffmanTree = new PQHeap(a.length);
-		// Creating a new Element for each of the
-		// 256 possible bit patterns (represented as an int)
-		// and inserts it into the PQHeap.
-		for (int i = 0; i < a.length; i++) {
-			Element tmp = new Element(a[i], new HuffmanTempTree(i));
-			HuffmanTree.insert(tmp);
-		}
-		// Runs the Huffman Algorithm which extracts the two
-		// smallest elements from the PQHeap and merges these into 
-		// a new element, which is then inserted into the PQHeap. 
-		// This is done n-1 = 255 times.
-		// Typecasting from Element.data (Object) to HuffmanTempTree
-		// is used.
-		for (int i = 0; i < a.length - 1; i++) {
-			Element x = HuffmanTree.extractMin();
-			Element y = HuffmanTree.extractMin();
-			int zFreq = x.key + y.key;
-			HuffmanTempTree ztree = new HuffmanTempTree(0);
-			ztree.merge((HuffmanTempTree) x.data, (HuffmanTempTree) y.data);
-			Element z = new Element(zFreq, ztree);
-			HuffmanTree.insert(z);
-		}
-		return HuffmanTree; 
-	}
-	// Method for extracting the Huffman Codes from a 
-	// Huffman Tree (PQHeap).
-	// Typecasting from Element.data (Object) to HuffmanTempTree
-	// is used.
-	public static String[]  makeHuffmanTable( PQHeap t ) {
-		String[] out = new String[256];
-		HuffmanTempTree huff = (HuffmanTempTree) t.extractMin().data;
-		return huff.inOrderTreeWalkPath(huff.root, "", out);
 	}
 }
